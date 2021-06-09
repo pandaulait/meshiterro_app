45f227d4b71ac9a09a374a7f448bf607603f7469
pandaulait <show.hey.panda@gmail.com>
2021-06-09 09:02:44 +0000

    [Finish]7-7

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,7 +4,7 @@ class PostImage < ApplicationRecord
   has_many :post_comments,dependent: :destroy
   has_many :favorites,dependent: :destroy
 
-  varidates :shop_name,presence: true
+  validates :shop_name,presence: true
   validates :image,presence: true
 
   def favorited_by?(user)
